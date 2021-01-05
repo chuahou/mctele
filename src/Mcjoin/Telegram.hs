9dd242e1d4491d80e501ef5ebc1f88c70bbde96a
Chua Hou <human+github@chuahou.dev>
2021-01-06 03:50:53 +0800

    Mcjoin.Telegram: rewrite for new 'ServerInfo'

--- src/Mcjoin/Telegram.hs
+++ src/Mcjoin/Telegram.hs
@@ -10,7 +10,6 @@ import           Mcjoin.Server
 
 import           Control.Monad           (void)
 import           Data.String.Interpolate (i)
-import           Data.Text               (unpack)
 import           Network.HTTP.Simple     (httpNoBody, parseRequest)
 
 type BotToken = String
@@ -22,13 +21,7 @@ sendServerStatus :: BotToken -> ChatID -> ServerInfo -> IO ()
 sendServerStatus tok chatId info = request >>= void . httpNoBody
     where
         request = parseRequest
-            [i|https://api.telegram.org/bot#{tok}/sendMessage?chat_id=#{chatId}&text=#{text}&parse_mode=MarkdownV2|]
-        text = if online info
-                  then case list $ players info of
-                         Just [] -> noPlayerText
-                         Just ps -> playersText ps
-                         Nothing -> noPlayerText
-                  else "Server is *offline*\\."
-        noPlayerText = "Server is *online* with no players\\."
-        playersText ps =
-            [i|Server is *online* with players: #{unwords . map unpack $ ps}\\.|]
+            [i|https://api.telegram.org/bot#{tok}/sendMessage?chat_id=#{chatId}&text=#{text info}&parse_mode=MarkdownV2|]
+        text (Just []) = "Server is *online* with no players\\."
+        text (Just ps) = [i|Server is *online* with players: *#{unwords ps}*\\.|]
+        text Nothing   = "Server is *offline*\\."

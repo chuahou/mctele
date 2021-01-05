ade263d1abc7f76a749d400a863a45c982a06724
Chua Hou <human+github@chuahou.dev>
2021-01-05 15:04:35 +0800

    nix: add haskell-language-server, envrc

--- flake.nix
+++ flake.nix
@@ -5,19 +5,39 @@
   description = "Simple Telegram bot that notifies when player joins server";
 
   inputs = {
-    # latest hs-updates branch for newest HLS
-    hs-updates.url = "nixpkgs/haskell-updates";
+    nixpkgs.url = "nixpkgs/nixos-20.09";
+
+    # hs-updates branch for HLS 0.7.1
+    hs-updates.url =
+      "github:nixos/nixpkgs?rev=a4530367944e55dbbd1d127123f3519b7483bc54";
   };
 
   outputs = { self, nixpkgs, hs-updates }:
     let
       system = "x86_64-linux";
-      pkgs   = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs { inherit system; overlays = [ hlsOverlay ]; };
+
+      hlsOverlay = self: super: {
+        haskellPackages = super.haskellPackages.override {
+          overrides = hsSelf: hsSuper: {
+            haskell-language-server =
+              (import hs-updates { inherit system; }).haskell-language-server;
+          };
+        };
+      };
 
       mcjoinhs = pkgs.haskell.lib.dontHaddock
         (pkgs.haskellPackages.callCabal2nix "mcjoinhs" ./. {});
 
     in rec {
       defaultPackage."${system}" = mcjoinhs;
+
+      devShell."${system}" =
+        (pkgs.haskell.lib.overrideCabal mcjoinhs (old: {
+          buildTools = (old.buildTools or []) ++ (with pkgs.haskellPackages; [
+            cabal-install
+            haskell-language-server
+          ]);
+        })).env;
     };
 }
